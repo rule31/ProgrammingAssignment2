30afb5f77009fd95d0451ec12381870b3bd1c134
mike c <mike.space.312@gmail.com>
2014-12-21 00:48:44 -0600

    comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a special object class that holds the methods and variable for
+## cached inverse object.
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
